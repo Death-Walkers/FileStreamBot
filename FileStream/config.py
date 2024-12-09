6bde8019c7c1ebc94befbfacc9023a1aaa7fb94f
Death-Walkers <royalashlynnofficial@gmail.com>
2024-12-09 07:51:28 +0530

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -8,7 +8,7 @@ class Telegram:
     API_HASH = str(env.get("API_HASH"))
     BOT_TOKEN = str(env.get("BOT_TOKEN"))
     OWNER_ID = int(env.get('OWNER_ID', '7427294551'))
-    WORKERS = int(env.get("WORKERS", "6"))  # 6 workers = 6 commands at once
+    WORKERS = int(env.get("WORKERS", "10"))  # 6 workers = 6 commands at once
     DATABASE_URL = str(env.get('DATABASE_URL'))
     UPDATES_CHANNEL = str(env.get('UPDATES_CHANNEL', "Telegram"))
     SESSION_NAME = str(env.get('SESSION_NAME', 'FileStream'))
@@ -34,7 +34,7 @@ class Server:
     NO_PORT = str(env.get("NO_PORT", "0").lower()) in ("1", "true", "t", "yes", "y")
     FQDN = str(env.get("FQDN", BIND_ADDRESS))
     URL = "http{}://{}{}/".format(
-        "s" if HAS_SSL else "", "web-rfleuqm0uq0i.up-de-fra1-k8s-1.apps.run-on-seenode.com", "" if NO_PORT else ":" + str(PORT)
+        "s" if HAS_SSL else "", "web-9gm6xv5tppfh.up-de-fra1-k8s-1.apps.run-on-seenode.com", "" if NO_PORT else ":" + str(PORT)
     )
 
 
